f2eda2ea97043f0402de3f4ae284cefca04b396c
HENRIQUE BOTELHO REIS MARQUES <23971603823@info.local>
2026-02-13 10:43:51 -0300

    :sparkles: ProdutoDto

--- VH_Burguer/Applications/Services/UsuarioService.cs
+++ VH_Burguer/Applications/Services/UsuarioService.cs
@@ -2,7 +2,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using VH_Burguer.Domains;
-using VH_Burguer.DTOs;
+using VH_Burguer.DTOs.Usuario;
 using VH_Burguer.Exceptions;
 using VH_Burguer.Interfaces;
 
